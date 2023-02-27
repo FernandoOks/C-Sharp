02f6afe55988f988b1ff897e91ce9b3d02a6bb6a
Fernando <ferjah13@gmail.com>
2023-02-27 10:14:53 -0300

    Interfaces

---
+++ VoceDisseInterfaces/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoceDisseInterfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var i = new ImpressoraComum();
+            var j = new ImpressoraCopiadora();
+            var c = new ImpressoraCopiadora();
+            Console.WriteLine(i.Imprimir("Estou estudando c#"));
+            Console.WriteLine(j.Copiar("Estou estudando e estou gostando"));
+
+            CopiarDocumento(new Xerox(), "Hello World!");
+        }
+
+        public static void CopiarDocumento(ICopiadora c, string texto)
+        {
+            Console.WriteLine("Estou copiando o texto a seguir: " + c.Copiar(texto));
+        }
+    }
+}
